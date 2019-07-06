0635e377fd8f1edf299f47d38d9c97d6e45aa389
Rocco Nicholls <rocco11nicholls@gmail.com>
2019-07-06 10:57:08 -0600

    Added generic email method

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,16 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'admin@rich-lawns.com'
   layout 'mailer'
 
+  def email(opts)
+    mail(
+      to:           opts[:to],
+      from:         opts[:from] || "admin@rich-lawns.com",
+      subject:      opts[:subject],
+      content_type: opts[:content_type],
+      body:         opts[:body]
+    )
+  end
+
   def deliver_email(email_id)
     email = Email.find(email_id).to_mail
     mail(
