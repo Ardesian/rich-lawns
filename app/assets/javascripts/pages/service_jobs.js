95193e90c78ff5f1f456420cebff32d9beb09885
Rocco Nicholls <rocco11nicholls@gmail.com>
2020-06-07 14:47:47 -0600

    Added live numbers while building an invoice

--- app/assets/javascripts/pages/service_jobs.js
+++ app/assets/javascripts/pages/service_jobs.js
@@ -1,14 +1,36 @@
 $(".ctr-service_jobs").ready(function() {
-  var duplicateOtherRow = function() {
+  var duplicateTemplateRow = function() {
     var template = document.querySelector("#extra-jobs-template")
     if (!template) { return }
     var content = document.importNode(template.content, true)
     $(".extra-jobs").append(content)
   }
-  duplicateOtherRow()
+  duplicateTemplateRow()
 
-  $(".extra-jobs").keypress(function() {
-    var empty = $(".extra-jobs").find(".other-field").filter(function() { return this.value == "" })
-    if (empty.length == 0) { duplicateOtherRow() }
+  $(document).on("keyup", ".item-fields", function() {
+    var isEmpty = $(".extra-jobs").find(".other-field").filter(function() { return this.value == "" })
+    if (isEmpty.length == 0) { duplicateTemplateRow() }
+
+    var invoice_total = 0
+
+    $(".item-fields").each(function() {
+      var unitField = $(this).find(".unit-field")
+      var unitCostField = $(this).find(".cost-field")
+      var totalField = $(this).find(".total-row")
+
+      var unitCount = parseInt(unitField.val())
+      var unitCost = parseInt(unitCostField.val())
+      var total = unitCount * unitCost
+
+      if (isNaN(total)) {
+        totalField.val("--")
+      } else {
+        invoice_total += total
+        totalField.val(total)
+      }
+    })
+
+    $("#invoice-total").val(invoice_total)
   })
+
 })
